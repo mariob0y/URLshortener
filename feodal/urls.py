211b67631ded76031e8d7d32b10573d99f634d51
mariob0y <69054485+mariob0y@users.noreply.github.com>
2021-02-15 17:25:33 +0200

    Update urls.py

--- feodal/urls.py
+++ feodal/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from core.views import UrlCreate, List, Stats
 from django.contrib.auth.decorators import login_required
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('/', login_required(List.as_view())),
+    path('/', RedirectView.as_view(url='/list')),
     path('', login_required(List.as_view())),
     path('s/', include('shortener.urls')),
     path('create/', login_required(UrlCreate.as_view())),
